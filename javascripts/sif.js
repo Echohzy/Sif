c8041be481e84024829563b8e30f643a46c8ee3d
ziyi <ziyi.huang@ceasia.com.cn>
2017-07-31 16:17:44 +0800

    add UA

--- javascripts/sif.js
+++ javascripts/sif.js
@@ -101,29 +101,216 @@ var SIF = (function(undefined){
 (function(S, undefined){
   var win = S.Env.host,
       navigator = win.navigator,
+      doc = win.document;
       ua = navigator.userAgent || "";
       
   function toNumber(str){
-    var count = 0;
-    str.replace(/\./g, function(){
+    var count = 0, version;
+    version = str.replace(/\./g, function(){
       count++;
       return count===1?".":"";
     });
-    return parseFloat(str);
+    return parseFloat(version);
   }
   
-  S.mix(S,{
-    UA:{
+
+  function getEnvironmentFromUserAgent(ua){
+    var os,shell,core,m, 
+      VERSION_PLACEHOLDER = '{{version}}',
+      IE_DETECT_TEMPLATE = '<!--[if IE ' + VERSION_PLACEHOLDER + ']><' + 'span></span><![endif]-->',
+      div = doc && doc.createElement('div'),
+      need_ie_detect = false;
+
+
+
+    var UA = {
+      /**
+       *  core for Safair, Chrome
+       */
+      webkit: undefined,
+      /**
+       * core for IE
+       */   
+      trident: undefined,
+      /**
+       * core for Firefox
+       */
+      gecko: undefined,
+      /**
+       * core for Opera
+       */
+      presto: undefined,
+      /**
+       * browser version
+       */
+      chrome: undefined,
+
+      safari: undefined,
+
+      firefox: undefined,
+
+      ie: undefined,
+
+      opera: undefined,
+      /**
+       * browser name
+       */
       shell: undefined,
+      /**
+       * operating system
+       */
       os: undefined,
-      core: undefined
+      /**
+       * mobile type, android or ios
+       */
+      mobile: undefined,
+
+      ipad: undefined,
+
+      ipod: undefined,
+
+      iphone: undefined,
+
+      /**
+       * phone system
+       */
+      ios: undefined,
+
+      android: undefined,
     }
-  });
+
+
+    //IE detect
+
+    if(div && div.getElementsByTagName){
+
+      div.innerHTML = IE_DETECT_TEMPLATE.replace(VERSION_PLACEHOLDER, function(){
+        return "";
+      });
+      if(div.getElementsByTagName("span").length){
+        need_ie_detect = true;
+      }
+    }
+
+    if(need_ie_detect){
+      UA[core = 'trident'] = 0.1;
+
+      if((m = ua.match(/Trident\/([\d.]*)/)) && m[1]){
+        UA[core] = toNumber(m[1]);
+      }
+
+      UA.core = core;
+
+      for(var v = 6, end=11; v<=end; v++){
+        div.innerHTML = IE_DETECT_TEMPLATE.replace(VERSION_PLACEHOLDER, function(){
+          return v;
+        });
+        if(div.getElementsByTagName('span').length>0){
+          UA[shell = "ie"] = v;
+          break;
+        }
+      }
+    } else {
+      if((m = ua.match(/AppleWebKit\/([\d.]*)/)) && m[1]) {
+        UA[core='webkit'] = toNumber(m[1]);
+
+        if((m = ua.match(/OPR\/([\d.]*)/)) && m[1]) {
+          UA[shell = 'opera'] = toNumber(m[1]);
+        }else if((m = ua.match(/Chrome\/([\d.]*)/)) && m[1]){
+          UA[shell = 'chrome'] = toNumber(m[1]);
+        }else if((m = ua.match(/\/([\d.]*) Safari/))&&m[1]){
+          UA[shell = 'safari'] = toNumber(m[1]);
+        }
+
+        if(/ Mobile/.test(ua) && ua.match(/iPad|iPod|iPhone/)){
+          UA.mobile = 'apple';
+
+          m = m.match(/OS ([^\s]*)/);
+          if(m && m[1]){
+            UA.ios = toNumber(m[1].replace("_", "."));
+          }
+          os = "ios";
+
+          m = ua.match(/iPad|iPod|iPhone/);
+
+          if(m && m[0]){
+            UA[m[0].toLowerCase()] = UA.ios;
+          }
+        }else if(/ Android/.test(ua)){
+          if(/Mobile/.test(ua)){
+            os = UA.mobile = 'android';
+          }
+          m = ua.match(/Android ([^\s]*);/)
+
+          if(m && m[1]){
+            UA.android = toNumber(m=[1]);
+          }
+        }
+        else if ((m = ua.match(/NokiaN[^\/]*|Android \d\.\d|webOS\/\d\.\d/))) {
+          UA.mobile = m[0].toLowerCase();
+        }
+      } else {
+        if((m = ua.match(/Presto\/([\d.]*)/)) && m[1]) {
+          UA[core = 'presto'] = toNumber(m[1]);
+
+          if ((m = ua.match(/Opera\/([\d.]*)/)) && m[1]) {
+            UA[shell = 'opera'] = toNumber(m[1]);
+
+            if ((m = ua.match(/Opera\/.* Version\/([\d.]*)/)) && m[1]) {
+              UA[shell] = toNumber(m[1]);
+            }
+            if ((m = ua.match(/Opera Mini[^;]*/)) && m) {
+                UA.mobile = m[0].toLowerCase(); 
+            }
+            else if ((m = ua.match(/Opera Mobi[^;]*/)) && m) {
+                UA.mobile = m[0];
+            }
+          }
+        } else {
+          if ((m = ua.match(/MSIE ([^;]*)|Trident.*; rv(?:\s|:)?([0-9.]+)/)) &&  (m[1] || m[2])) {
+              UA[shell="ie"] = toNumber(m[1]||m[2]);
+          }else{
+            if ((m = ua.match(/Gecko/))) {
+              UA[core = 'gecko'] = 0.1; 
+              if ((m = ua.match(/rv:([\d.]*)/)) && m[1]) {
+                  UA[core] = toNumber(m[1]);
+                  if (/Mobile|Tablet/.test(ua)) {
+                      UA.mobile = 'firefox';
+                  }
+              }
+              if ((m = ua.match(/Firefox\/([\d.]*)/)) && m[1]) {
+                  UA[shell = 'firefox'] = toNumber(m[1]);
+              }
+            }
+          }
+        }
+      }
+    }
+
+    if(!os){
+      if((/windows|win32/i).test(ua)){
+        os = "windows";
+      } else if ((/macintosh|mac_powerpc/i).test(ua)) {
+        os = "macintosh";
+      } else if ((/linux/i).test(ua)) {
+        os = 'linux';
+      } else if ((/rhino/i).test(ua)) {
+        os = 'rhino';
+      }
+    }
+
+    UA.os = os;
+    UA.core = core;
+    UA.shell  = shell;
+
+    return UA;
+  }
   
-  
-  
-  
-  
+  S.mix(S, {
+    UA: getEnvironmentFromUserAgent(ua)
+  })
+
+
 })(SIF)
 
 
