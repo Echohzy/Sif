021dbeae5d9460527105613fe7234cd3251d4b16
Echohzy <1820123540@qq.com>
2017-08-14 23:36:31 +0800

    promise

--- javascripts/index.js
+++ javascripts/index.js
@@ -2,15 +2,16 @@ SIF.require(["javascripts/Promise"], function(Promise){
   var promise1 = new Promise(function(resolve, reject){
     setTimeout(function(){
       resolve("promise1");
-    }, 1000)
-  }).then(function(data){return data+"ext"});
-  var promise2 = new Promise(function(resolve, reject){
-    setTimeout(function(){
-      resolve("jajaja")
-    }, 2000)
+      console.log("1");
+    },2000)
   });
 
-  Promise.race([promise1, promise2]).then(function(data){
-    console.log(data+" is winner");
+  var promise2 = new Promise(function(resolve, reject){
+    setTimeout(function(){
+      resolve("promise2");
+      console.log("2")
+    },1000)
   });
+
+  Promise.resolve().then(() => promise1).then(text => console.log(text)).then(()=>promise2).then(text=>console.log(text));
 });
