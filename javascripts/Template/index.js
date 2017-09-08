1a96795a803e7aeee6e6ace1cde36a1b48efd9b4
Echohzy <1820123540@qq.com>
2017-09-08 23:27:30 +0800

    detail

--- javascripts/Template/index.js
+++ javascripts/Template/index.js
@@ -2,34 +2,50 @@
 
 SIF.define("javascripts/Template", function(){
   
+  function parseJs(str){
+    var reg = /^=(.*)$/,match;
+    if(match=str.match(reg)){
+      return match[0];
+    }
+    return str;
+  }
   function parse(tpl){
     var code = 'var _code = "";\n';
+    tpl = tpl.replace(/('|")/g, "\$1").replace(/(\n|\r)/g, " ");
     var lines = tpl.split("<%");
+    var reg = /^=(.*)$/;
     for(var i=0,len=lines.length;i<len;i++){
       var pices = lines[i].split("%>");
-      for(var j=0,l=pices.length;j<l;j++){
-        code += '_code+="'+pices[j].replace(/\n/g,"").replace(/r/g,"")+'";\n';
+      if(pices.length===1){
+        code += '_code+="'+pices[0]+'";\n';
+      }else{
+        code += parseJs(pices[0])+"\n";
+        if(pices[1]){
+          code += '_code+="'+pices[1]+'";\n';
+        }
       }
     }
+    code += 'return _code;';
     return code;
     
   }
 
   function compile(tpl, data){
-    var header = '(function(){\n'+
-    'var str = ""'+
+    var header = 'var html = (function(data){\n'+
+    'var str = "";'+
     'for(var key in data){'+
-    'str+="var "+ key + "="+ data[key]+";"'+
-    '}\neval(str)';
-    var footer = '\n})(tpl)';
+    'str+="var "+ key + "="+ data[key]+";";'+
+    '}\neval(str);';
+    var footer = '\n})(data);'+
+    'return html;';
     var mainCode = parse(tpl);
-    return new Function("tpl", header+mainCode+footer);
+    return new Function("data", header+mainCode+footer);
   }
 
 
   function template(tpl, data){
     var Render = compile(tpl);
-    Render(tpl);
+    return Render(data);
   }
 
 
--- javascripts/index.js
+++ javascripts/index.js
@@ -1,6 +1,7 @@
 
 SIF.require(["javascripts/Promise", "javascripts/Fetch", "javascripts/Observer", "javascripts/Lazyload", "javascripts/Template"], function(Promise, Fetch, Observer, Lazyload, Template){
   
-  Template(document.getElementById("template").innerHTML);
+  var t = Template(document.getElementById("template").innerHTML,{});
+  console.log(t);
  
 });
